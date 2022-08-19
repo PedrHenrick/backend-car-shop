8faeb8f3f350daca47ceb18350d926505a26a0f9
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-19 18:01:30 -0300

    fix: :bug: Corrindo erro na escrita do atributo da interface ICar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -3,7 +3,7 @@ import { IVehicleZodShema } from './IVehicle';
 
 export const ICarZodShema = z.object({
   doorsQty: z.number().min(2).max(4),
-  seatsWty: z.number().min(2).max(7),
+  seatsQty: z.number().min(2).max(7),
 }).merge(IVehicleZodShema);
 
 export type ICar = z.infer<typeof ICarZodShema>;
