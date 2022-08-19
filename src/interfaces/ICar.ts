e1f32fdd65233e805304d74e1303dc7d8d2d338a
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-19 17:50:44 -0300

    feat(interface): :sparkles: Criando a interface ICar
    Interface que cria uma schema para validação
    
    Essa interface faz um merge com a interface IVehicle

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { IVehicleZodShema } from './IVehicle';
+
+export const ICarZodShema = z.object({
+  doorsQty: z.number().min(2).max(4),
+  seatsWty: z.number().min(2).max(7),
+}).merge(IVehicleZodShema);
+
+export type ICar = z.infer<typeof ICarZodShema>;
