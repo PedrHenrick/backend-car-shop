fbc955259ab71f44148058df9d82220b774cf41a
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-20 12:46:19 -0300

    feat(middleware): :sparkles: Criando middleware de erro

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,27 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
+import { errorCatalog, ErrorTypes } from '../class/error.class';
+
+const errorHandle: ErrorRequestHandler = (
+  error: Error | ZodError,
+  _request: Request,
+  response: Response,
+  _next: NextFunction,
+) => {
+  if (error instanceof ZodError) {
+    return response.status(400).json({ message: error.issues });
+  }
+
+  const messageAsErrorType = error.message as keyof typeof ErrorTypes;
+  const mappedError = errorCatalog[messageAsErrorType];
+
+  if (mappedError) {
+    const { status, message } = mappedError;
+    return response.status(status).json({ message });
+  }
+
+  console.log(error);
+  return response.status(500).json({ message: 'Internal Error' });
+};
+
+export default errorHandle;
