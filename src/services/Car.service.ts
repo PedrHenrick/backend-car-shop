4affaa15ddc3ea399eff67c314df05da14d118a1
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-20 13:12:18 -0300

    refactor(service): :recycle: Tornando o código da service mais limpo

--- src/services/Car.service.ts
+++ src/services/Car.service.ts
@@ -3,14 +3,11 @@ import { ICar, ICarZodShema } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
 
 export default class CarService implements IService<ICar> {
-  private _car: IModel<ICar>;
-
-  constructor(model: IModel<ICar>) { this._car = model; }
+  constructor(private _model: IModel<ICar>) { }
 
   public async create(carObject: ICar): Promise<ICar> {
     const parsed = ICarZodShema.safeParse(carObject);
-
     if (!parsed.success) throw parsed.error;
-    return this._car.create(carObject);
+    return this._model.create(carObject);
   }
 }
