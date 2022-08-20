426441d5ef046d6a5a1509c6e6f3c03fa61dfa1c
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-20 12:46:56 -0300

    feat(app): :sparkles: Adicionando express.json e middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import 'express-async-errors';
 import express from 'express';
 import router from './routes/Router';
+import errorHandle from './middlewares/error.middleware';
 
 const app = express();
+app.use(express.json());
 app.use(router);
+app.use(errorHandle);
 
 export default app;
