2e691ddd46cd43bf7fd9c3648b3c3f516e760744
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 19:09:22 -0300

    test(test-service): :test_tube: Adicionando testes da camada service da rota /cars

--- src/tests/unit/models/car.model.test.ts
+++ src/tests/unit/models/car.model.test.ts
@@ -1,8 +1,9 @@
 import * as sinon from 'sinon';
 import chai from 'chai';
 import { Model } from 'mongoose';
-import { objectForAddACar, successfulCreationReturn } from '../../mocks/car.mock';
 import CarModel from '../../../models/Car.model';
+import { objectForAddACar, successfulCreationReturn } from '../../mocks/car.mock';
+
 const { expect } = chai;
 
 describe('Testando camada models da rota /cars', () => {
@@ -18,7 +19,7 @@ describe('Testando camada models da rota /cars', () => {
   })
 
   describe('Testando se é possível adicionar um novo carro', async () => {
-    it('Testando se passa co sucesso', async () => {
+    it('Testando se é possível adicionar com sucesso', async () => {
       const resultOfAddingTheCar = await carModel.create(objectForAddACar);
       expect(resultOfAddingTheCar).to.be.deep.equal(successfulCreationReturn);
     });
--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,22 +1,38 @@
-// template para criação dos testes de cobertura da camada de service
+import * as sinon from 'sinon';
+import chai from 'chai';
+import { ZodError } from 'zod';
+import CarModel from '../../../models/Car.model';
+import CarService from '../../../services/Car.service';
+import { objectForAddACar, successfulCreationReturn } from '../../mocks/car.mock';
 
+const { expect } = chai;
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+describe('Testando camada service da rota /cars', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
 
-// describe('Sua descrição', () => {
+  before(async () => {
+    sinon
+      .stub(carModel, 'create')
+      .resolves(successfulCreationReturn);
+  });
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  after(()=>{
+    sinon.restore();
+  })
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  describe('Testando se é possível adicionar um novo carro', () => {
+    it('Testando se é possível adicionar com sucesso', async () => {
+      const resultOfAddingTheCar = await carService.create(objectForAddACar);
+      expect(resultOfAddingTheCar).to.be.deep.equal(successfulCreationReturn);
+    });
 
-//   it('', async () => {});
-
-// });
+    it('Testando se não é possível adicionar passando dados inválidos', async () => {
+      try {
+        await carService.create({} as any);
+      } catch (error) {
+        expect(error).to.be.instanceOf(ZodError);
+      }
+    });
+  });
+});
