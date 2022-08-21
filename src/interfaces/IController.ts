d7c92ba823a8fb8ceb24d54f45622d56a131c254
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 19:43:10 -0300

    feat(interface): :sparkles: Liberando as funções read nas interfaces IController e IService

--- src/interfaces/IController.ts
+++ src/interfaces/IController.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 
 export interface IController<T> {
   create(request: Request, response: Response<T>): Promise<Response>,
+  read(request: Request, response: Response<T[]>): Promise<Response>,
   readOne?(request: Request, response: Response<T>): Promise<Response>,
-  read?(request: Request, response: Response<T[]>): Promise<Response>,
   update?(request: Request, response: Response<T>): Promise<Response>,
   delete?(request: Request, response: Response<T>): Promise<Response>,
 }
