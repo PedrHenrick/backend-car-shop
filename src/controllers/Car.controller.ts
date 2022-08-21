5e1a0ec963be8aa10b6e3e98128b9a30e24f259b
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 20:47:42 -0300

    feat(controller): :sparkles: Criando função readOne no controller da rota /cars/:id
    Adicionando também a melhoria da tipagem

--- src/controllers/Car.controller.ts
+++ src/controllers/Car.controller.ts
@@ -9,13 +9,24 @@ export default class CarController implements IController<ICar> {
   public async create(
     request: Request & { body: ICar },
     response: Response<ICar>,
-  ) {
+  ): Promise<Response> {
     const resultOfAddingTheCar = await this._service.create(request.body);
     return response.status(201).json(resultOfAddingTheCar);
   }
 
-  public async read(_request: Request, response: Response<ICar[]>) {
+  public async read(
+    _request: Request,
+    response: Response<ICar[]>,
+  ): Promise<Response> {
     const allCars = await this._service.read();
     return response.status(200).json(allCars);
+  }
+
+  public async readOne(
+    request: Request & { params: { id: string } },
+    response: Response<ICar | null>,
+  ): Promise<Response> {
+    const car = await this._service.readOne(request.params.id);
+    return response.status(200).json(car);
   }
 }
