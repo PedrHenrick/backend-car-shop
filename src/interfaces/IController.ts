fbb599231d6c4c9eb7699427373d630ca8e4783b
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 20:46:05 -0300

    perf(interface): :zap: Adicionando alterações nas interfaces IController e IService
    Melhorando a tipagem dos itens e liberando a função readOne

--- src/interfaces/IController.ts
+++ src/interfaces/IController.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express';
 
 export interface IController<T> {
-  create(request: Request, response: Response<T>): Promise<Response>,
-  read(request: Request, response: Response<T[]>): Promise<Response>,
-  readOne?(request: Request, response: Response<T>): Promise<Response>,
-  update?(request: Request, response: Response<T>): Promise<Response>,
-  delete?(request: Request, response: Response<T>): Promise<Response>,
+  create(
+    request: Request & { body: T },
+    response: Response<T>): Promise<Response>,
+  read(
+    request: Request,
+    response: Response<T[]>): Promise<Response>,
+  readOne(
+    request: Request & { params: { id: string } },
+    response: Response<T | null>): Promise<Response>,
+  update?(
+    request: Request,
+    response: Response<T | null>): Promise<Response>,
+  delete?(
+    request: Request,
+    response: Response<T | null>): Promise<Response>,
 }
