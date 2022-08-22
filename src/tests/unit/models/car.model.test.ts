0fcc13c3752dd46ad9697fecff60aee350962547
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 21:41:57 -0300

    test(test-model): :test_tube: Adicionando testes para as funções read e readOne na model da rota /cars

--- src/tests/unit/models/car.model.test.ts
+++ src/tests/unit/models/car.model.test.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 import { Model } from 'mongoose';
 import CarModel from '../../../models/Car.model';
-import { objectForAddACar, successfulCreationReturn } from '../../mocks/car.mock';
+import { objectForAddACar, returnOfAllCars, returnOneCars, successfulCreationReturn } from '../../mocks/car.mock';
 
 const { expect } = chai;
 
@@ -12,6 +12,12 @@ describe('Testando camada models da rota /cars', () => {
     sinon
       .stub(Model, 'create')
       .resolves(successfulCreationReturn);
+    sinon
+      .stub(Model, 'find')
+      .resolves(returnOfAllCars);
+    sinon
+      .stub(Model, 'findOne')
+      .resolves(returnOneCars);
   });
 
   after(()=>{
@@ -22,6 +28,20 @@ describe('Testando camada models da rota /cars', () => {
     it('Testando se é possível adicionar com sucesso', async () => {
       const resultOfAddingTheCar = await carModel.create(objectForAddACar);
       expect(resultOfAddingTheCar).to.be.deep.equal(successfulCreationReturn);
+    });
+  });
+
+  describe('Testando se retorna todos os carro', async () => {
+    it('Testando se retorna com sucesso', async () => {
+      const allCars = await carModel.read();
+      expect(allCars).to.be.deep.equal(returnOfAllCars);
+    });
+  });
+
+  describe('Testando se retorna um carro específico', async () => {
+    it('Testando se passando um id válido retorna com sucesso', async () => {
+      const car = await carModel.readOne('4edd40c86762e0fb12000003');
+      expect(car).to.be.deep.equal(returnOneCars);
     });
   });
 });
