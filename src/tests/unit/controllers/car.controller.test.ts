9d89d16d92a042e1169789ad22f170a893806c70
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 21:43:17 -0300

    test(test-controller): :test_tube: Adicionando testes para as funções read e readOne no controller da rota /cars

--- src/tests/unit/controllers/car.controller.test.ts
+++ src/tests/unit/controllers/car.controller.test.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 import CarModel from '../../../models/Car.model';
 import CarService from '../../../services/Car.service';
-import { objectForAddACar, successfulCreationReturn } from '../../mocks/car.mock';
+import { objectForAddACar, returnOfAllCars, returnOneCars, successfulCreationReturn } from '../../mocks/car.mock';
 import CarController from '../../../controllers/Car.controller';
 import { Request, Response } from 'express';
 
@@ -20,6 +20,12 @@ describe('Testando camada controller da rota /cars', () => {
     sinon
       .stub(carModel, 'create')
       .resolves(successfulCreationReturn);
+    sinon
+      .stub(carModel, 'read')
+      .resolves(returnOfAllCars);
+    sinon
+      .stub(carModel, 'readOne')
+      .resolves(returnOneCars);
 
     response.status = sinon.stub().returns(response);
     response.json = sinon.stub().returns(response);
@@ -36,6 +42,25 @@ describe('Testando camada controller da rota /cars', () => {
       
       expect((response.status as sinon.SinonStub).calledWith(201)).to.be.true;
       expect((response.json as sinon.SinonStub).calledWith(successfulCreationReturn)).to.be.true;
+    });
+  });
+
+  describe('Testando se retorna todos os carro', async () => {
+    it('Testando se retorna com sucesso', async () => {
+      await carController.read(request, response);
+      
+      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((response.json as sinon.SinonStub).calledWith(returnOfAllCars)).to.be.true;
+    });
+  });
+
+  describe('Testando se retorna um carro específico', async () => {
+    it('Testando se passando um id válido retorna com sucesso', async () => {
+      request.params = { id: returnOneCars._id }
+      await carController.readOne(request, response);
+      
+      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((response.json as sinon.SinonStub).calledWith(returnOneCars)).to.be.true;
     });
   });
 });
