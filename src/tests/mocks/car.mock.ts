9a0a8e9adcb37b83647740f67ac92fffb727f72c
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 21:40:36 -0300

    feat(mockup): :sparkles: Adicionando novas funções no mockup da rota /cars

--- src/tests/mocks/car.mock.ts
+++ src/tests/mocks/car.mock.ts
@@ -18,3 +18,34 @@ export const successfulCreationReturn: ICar & { _id: string } = {
   seatsQty: 2,
   doorsQty: 2
 };
+
+export const returnOfAllCars: (ICar & { _id: string })[] = [
+  {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  },
+  {
+    _id: "4edd40c86762e0fb12000004",
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  },
+];
+
+export const returnOneCars: ICar & { _id: string } = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+};
