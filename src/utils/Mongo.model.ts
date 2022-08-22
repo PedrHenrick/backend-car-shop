77c2fc11b1d0bf23215eef5d3ab35a6c177d2764
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-21 23:15:07 -0300

    fix: :bug: Removendo tag __v e alterando mensagem de erro do arquivo Mongo.model

--- src/utils/Mongo.model.ts
+++ src/utils/Mongo.model.ts
@@ -14,26 +14,26 @@ export default abstract class MongoModel<T> implements IModel<T> {
   }
 
   public async read():Promise<T[]> {
-    return this._model.find();
+    return this._model.find().select('-__v');
   }
 
   public async readOne(_id: string): Promise<T | null> {
     if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
-    return this._model.findOne({ _id });
+    return this._model.findOne({ _id }).select('-__v');
   }
 
   public async update(_id: string, obj: Partial<T>): Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('Invalid Mongo Id');   
+    if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);   
     return this._model.findByIdAndUpdate(
       { _id },
       { ...obj } as UpdateQuery<T>,
       { new: true },
-    );
+    ).select('-__v');
   }
 
   public async delete(_id: string): Promise<T | null> {
-    if (!isValidObjectId(_id)) throw Error('Invalid Mongo Id');
+    if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
     
-    return this._model.findByIdAndRemove({ _id });
+    return this._model.findByIdAndRemove({ _id }).select('-__v');
   }
 }
